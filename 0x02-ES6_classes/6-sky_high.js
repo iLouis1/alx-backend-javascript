43dd0ba6e20d6df920785c356906e0ca6f2a95a9
iLouis1 <rhiganakatugba@gmail.com>
2024-06-27 15:44:15 -0700

    Added

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+0-classroom.js
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
