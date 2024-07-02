1c919a858276117f22ff7002a1d7e26d079ea834
iLouis1 <rhiganakatugba@gmail.com>
2024-07-02 15:29:25 +0000

    3. Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+0-get_list_students.js
+const getStudentIdsSum = (students) => {
+  const id = students.reduce(
+    (acc, value) => acc + value.id, 0,
+  );
+  return id;
+};
+
+export default getStudentIdsSum;
